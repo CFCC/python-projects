acbb3245044e1c5913fa31a2bdefc1bece88f1f7
No-This-Is-Patrick <bryantp@trinityhillers.net>
2017-06-15 10:48:07 -0400

    Added pygame base

--- camper-project-color-tag/Client.py
+++ camper-project-color-tag/Client.py
@@ -1,1 +1,22 @@
 import socket
+import pygame
+import pickle
+version = "1.0"
+pygame.init()
+WHITE = (255, 255, 255)
+size = (1000, 800)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Color Tag")
+done = False
+clock = pygame.time.Clock()
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(WHITE)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+pygame.quit()
