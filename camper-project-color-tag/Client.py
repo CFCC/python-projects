217d08b6161c49d3b99dec14e2dce0df55b522b7
No-This-Is-Patrick <bryantp@trinityhillers.net>
2017-06-15 10:58:02 -0400

    Added pygame base

--- camper-project-color-tag/Client.py
+++ camper-project-color-tag/Client.py
@@ -4,6 +4,7 @@ import pickle
 version = "1.0"
 pygame.init()
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 size = (1000, 800)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Color Tag")
