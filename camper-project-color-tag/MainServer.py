e5651d67072fa1bcd28c99321e8df5c245c9cd24
Logan <kingofallcreepers@gmail.com>
2017-06-16 09:24:52 -0400

    Finished Server

--- camper-project-color-tag/MainServer.py
+++ camper-project-color-tag/MainServer.py
@@ -51,13 +51,24 @@ def Server():
     _thread.start_new_thread(Player_Join, ())
     while not done:
         for x in All:
-            info = Information[x[0]][1]
-            if info[2] > -1:
-                if info[2] == Information[info[2]][1][2]:
-                    newTeam = x[1]+1
-                    if newTeam > 2:
-                        newTeam = 0
-                    All[info[2]][1] = newTeam
+            if Information[x[0]] != ["disconnected", "disconnected"]:
+                if x[3] > 0:
+                    x[3] -= 1
+                info = Information[x[0]][1]
+                if info[2] > -1:
+                    if info[2] == Information[info[2]][1][2]:
+                        newTeam = x[1]+1
+                        if newTeam > 2:
+                            newTeam = 0
+                        All[info[2]][1] = newTeam
+                        All[info[2]][3] = 300
+
+                sendList = [[x[1], x[3]],[]]
+                tempList = All.remove(x)
+                for y in tempList:
+                    genList = [y[2], y[0], Information[y[0]][0], Information[y[0]][1], y[3]]
+                    sendList[1].append(genList)
+                Information[x[0][0]] = sendList
 
 
 
