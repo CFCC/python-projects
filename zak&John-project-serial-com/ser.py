69e3a3ff764e10f02664f8f16b07529a9c812968
ZakHarriman <zak.harriman@epicgeek.info>
2017-06-23 13:53:26 -0400

    no message

--- zak&John-project-serial-com/ser.py
+++ zak&John-project-serial-com/ser.py
@@ -1,5 +1,10 @@
 import serial
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
-print(ser.name)         # check which port was really used
-ser.write(b'hello')     # write a string
-ser.close()             # close port
+
+ser = serial.Serial('/dev/ttyUSB0')  # This format is specific to Debian Linux environments
+print(ser.name)          # check which port was really used
+ser.write('hello\n')     # write a string
+ser.close()              # close port
+
+ser = serial.Serial('COM3', 38400, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
+s = ser.read(100)       # read up to one hundred bytes
+ser.close()              # close port
